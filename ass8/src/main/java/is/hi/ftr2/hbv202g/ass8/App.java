8c75e05fd124c3d300c978eddf1391ab6b9bab71
Fergal Riordan <riordanf@tcd.ie>
2023-03-09 22:07:04 +0000

    Initial commit

---
+++ ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/App.java
@@ -0,0 +1,13 @@
+package is.hi.ftr2.hbv202g.ass8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ass8/src/test/java/is/hi/ftr2/hbv202g/ass8/AppTest.java
@@ -0,0 +1,20 @@
+package is.hi.ftr2.hbv202g.ass8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
