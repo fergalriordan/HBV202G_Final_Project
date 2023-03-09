362d29b15544e0c2706329f77ee3755881ca12ab
Fergal Riordan <riordanf@tcd.ie>
2023-03-09 23:09:42 +0000

    Author, User, Student and Faculty Member added

---
+++ ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/FacultyMember.java
@@ -0,0 +1,18 @@
+package is.hi.ftr2.hbv202g.ass8;
+
+public class FacultyMember extends User{
+    private String department;
+    
+    public FacultyMember(String name, String department) {
+        super(name);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
