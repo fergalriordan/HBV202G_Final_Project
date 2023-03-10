e91169dba63b3702c1bdded2a4f346edcad5185a
Fergal Riordan <riordanf@tcd.ie>
2023-03-10 12:54:53 +0000

    Completed LibrarySystem implementation

--- ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/App.java
+++ ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/App.java
@@ -1,9 +1,8 @@
 package is.hi.ftr2.hbv202g.ass8;
 
-/**
- * Hello world!
- *
- */
+// FOR END OF TERM PROJECT: 
+// Update this main to inlcude a front-end for the library system.
+
 public class App 
 {
     public static void main( String[] args )
--- ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/LibrarySystem.java
+++ ass8/src/main/java/is/hi/ftr2/hbv202g/ass8/LibrarySystem.java
@@ -1,37 +1,88 @@
 package is.hi.ftr2.hbv202g.ass8;
 
-import java.util.List;
+import java.util.*;
 import java.time.LocalDate;
 
 public class LibrarySystem {
     private List<User> users;
     private List<Book> books;
     private List<Lending> lendings;
-    
-    private String title;
-    private List<Author> authors;
-    private String name; 
-    private boolean feePaid;
-    private String department;
-    private LocalDate dueDate;
 
     public LibrarySystem() {
-        this.users = new ArrayList<>(); // wrong
-        this.books = new ArrayList<>(); // wrong
-        this.lendings = new ArrayList<>(); // wrong
-        this.title = "";
-        this.authors = new ArrayList<>(); // wrong
-        this.name = "";
-        this.feePaid = false;
-        this.department = "";
-        this.dueDate = null;
+        this.users = new ArrayList<User>();
+        this.books = new ArrayList<Book>(); 
+        this.lendings = new ArrayList<Lending>();
     }
 
     public void addBookWithTitleAndAuthorList (String title, List<Author> authors) throws EmptyAuthorListException {
         if (authors.size() == 0) {
             throw new EmptyAuthorListException("'Authors' field cannot be empty");
         }
-        this.title = title;
-        this.authors = authors;
+        Book book = new Book(title, authors);
+        this.books.add(book);
     }
+
+    public void addStudentUser(String user, boolean feePaid){
+        this.users.add(new Student(user, feePaid));
+    }
+
+    public void addFacultyMemberUser(String name, String department){
+        this.users.add(new FacultyMember(name, department));
+    }
+
+    public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
+        Book found = null;
+        for (Book book : this.books) {
+            if (book.getTitle().equals(title)) {
+                found = book;
+            }
+        }
+        if (found == null) {
+            throw new UserOrBookDoesNotExistException("Book with title '" + title + "' does not exist");
+        }
+        return found;
+    }
+
+    public User findUserByName(String name) throws UserOrBookDoesNotExistException {
+        User found = null;
+        for (User user : this.users) {
+            if (user.getName().equals(name)) {
+                found = user;
+            }
+        }
+        if (found == null) {
+            throw new UserOrBookDoesNotExistException("User with name '" + name + "' does not exist");
+        }
+        return found;
+    }
+
+    public void borrowBook(User user, Book book) {
+        this.lendings.add(new Lending(book, user));
+    }
+
+    public void extendLending(FacultyMember facultyMember, Book book, LocalDate newDueDate) {
+        // find the lending relating to the given faculty member and given book
+        Lending foundLending = null;
+        for (Lending lending : this.lendings) {
+            if (lending.getUser().equals(facultyMember) && lending.getBook().equals(book)) {
+                foundLending = lending;
+            }
+        }
+        // update the due date of this lending to the new due date
+        foundLending.setDueDate(newDueDate);
+    }
+
+    public void returnBook(User user, Book book) {
+        // find the lending relating to the given user and given book 
+        Lending foundLending = null;
+        for (Lending lending : this.lendings) {
+            if (lending.getUser().equals(user) && lending.getBook().equals(book)) {
+                foundLending = lending;
+            }
+        }
+        // remove the lending from the lendings list
+        this.lendings.remove(foundLending);
+    }
+
+
 }
